8df019ff28031bbfd4bf03ebba8325350a0e2791
anderdc <alexanderdcastaneda@gmail.com>
2022-06-25 17:21:22 -0500

    small bug fix

--- app/__init__.py
+++ app/__init__.py
@@ -10,8 +10,8 @@ from playhouse.shortcuts import model_to_dict
 
 #MAKE SURE TO START SQL SERVER W/ COMMAND: sudo /etc/init.d/mysql start
 #creates a database object from .env file
-# database = MySQLDatabase(os.getenv('MYSQL_DATABASE'), user=os.getenv('MYSQL_USER'), password=os.getenv('MYSQL_PASSWORD'), host=os.getenv('MYSQL_HOST'), port=3306)
-db = MySQLDatabase('myportfoliodb', user='myportfolio', password='mypassword', host='localhost', port=3306)
+db = MySQLDatabase(os.getenv('MYSQL_DATABASE'), user=os.getenv('MYSQL_USER'), password=os.getenv('MYSQL_PASSWORD'), host=os.getenv('MYSQL_HOST'), port=3306)
+#db = MySQLDatabase('myportfoliodb', user='myportfolio', password='mypassword', host='localhost', port=3306)
 print(db)
 
 class TimelinePost(Model):
